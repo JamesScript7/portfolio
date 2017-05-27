74b6aa4d6435baaef3fca1f58c3fc4558943f0bc
James Kim <yameskim@gmail.com>
2017-05-27 15:48:07 -0400

    Need to complete about and works

--- scripts.js
+++ scripts.js
@@ -1,23 +1,30 @@
 'use strict';
 
 (function() {
-
   // Parallax effect
-  var velocity = 0.1;
+  var velocity = 0.2;
   function update() {
     var pos = $(window).scrollTop();
 
     $('#container').each(function() {
       var $element = $(this);
-      var height = $element.height() - 1585;
-      console.log(height);
-      $(this).css('background-position', '10% ' + Math.round((height - pos) * velocity) + 'px');
+      // var height = $element.height();
+      $(this).css('background-position', '0% ' + Math.round(((-0) - pos) * velocity) + 'px');
     });
   };
 
   $(window).bind('scroll', update);
 
-  // etc.
-
+  (function($) {
+    $(document).ready(function(){
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 20) {
+          $('#nav').fadeIn(500);
+        } else {
+          $('#nav').fadeOut(500);
+        }
+      });
+    });
+  })(jQuery);
 
 })();

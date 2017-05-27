a9846cf54ed593de7d7fa930b1cbdf1c159c5f71
James Kim <yameskim@gmail.com>
2017-05-26 23:36:17 -0400

    added skills and working on footer

--- scripts.js
+++ scripts.js
@@ -3,13 +3,14 @@
 (function() {
 
   // Parallax effect
-  var velocity = 0.05;
+  var velocity = 0.1;
   function update() {
     var pos = $(window).scrollTop();
 
     $('#container').each(function() {
       var $element = $(this);
       var height = $element.height() - 1585;
+      console.log(height);
       $(this).css('background-position', '10% ' + Math.round((height - pos) * velocity) + 'px');
     });
   };

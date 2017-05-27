c4fd3cde64fe15f76366715c1d2724ada734bb96
James Kim <yameskim@gmail.com>
2017-05-27 16:45:12 -0400

    fine tuned

--- scripts.js
+++ scripts.js
@@ -2,22 +2,22 @@
 
 (function() {
   // Parallax effect
-  var velocity = 0.2;
+  var velocity = 0.4;
   function update() {
     var pos = $(window).scrollTop();
 
     $('#container').each(function() {
       var $element = $(this);
       // var height = $element.height();
-      $(this).css('background-position', '0% ' + Math.round(((-0) - pos) * velocity) + 'px');
+      $(this).css('background-position', '0% ' + Math.round((-pos) * velocity) + 'px');
     });
   };
 
   $(window).bind('scroll', update);
 
   (function($) {
-    $(document).ready(function(){
-      $(window).scroll(function(){
+    $(document).ready(function() {
+      $(window).scroll(function() {
         if ($(this).scrollTop() > 20) {
           $('#nav').fadeIn(500);
         } else {

dee5fd6f2210b983608fe67d634075d1fb08ae13
James Kim <yameskim@gmail.com>
2018-03-22 21:44:26 -0400

    Set overflow to hidden

--- galaxy-version/src/main.js
+++ galaxy-version/src/main.js
@@ -1,6 +1,6 @@
-function setup() {
-  createCanvas(800,400);
-}
-function draw() {
-  background(255,255,255);
-}
+// function setup() {
+//   createCanvas(800,400);
+// }
+// function draw() {
+//   background(255,255,255);
+// }

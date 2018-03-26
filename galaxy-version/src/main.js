dc8c5449098e07abc5dac59df468511add20e659
James Kim <yameskim@gmail.com>
2018-03-26 04:00:47 -0400

    About me done

--- galaxy-version/src/main.js
+++ galaxy-version/src/main.js
@@ -6,20 +6,29 @@
 // }
 window.onload = function() {
   var forest = document.getElementById('forest');
+  var wrapper = document.getElementById('wrapper');
+  var catPizza = document.getElementById('flying-cat-pizza');
   var innerHeight = window.innerHeight;
-  console.log(innerHeight);
+  // console.log(innerHeight);
 
   window.addEventListener('scroll', function(e) {
-    console.log(window.scrollY);
-    if (window.scrollY > innerHeight / 3) {
+    // console.log(window.scrollY);
+    if (window.scrollY > (innerHeight / 6)) {
       forest.style.display = 'inline-block';
+      if (window.scrollY > (innerHeight / 4)) {
+        wrapper.style.animation = 'fadeToBlack 1.5s forwards'
+      } else {
+        wrapper.style.animation = 'none';
+      }
     } else {
+      wrapper.style.animation = 'none';
       forest.style.display = 'none';
     }
+
+    // if (catPizza.scrollIntoView()) {
+    //   console.log('start the animation!')
+    // }
   });
-
-
-
 
 
 

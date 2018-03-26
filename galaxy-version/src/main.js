646f74c3a82c048bee7e0d1aa3bafa5906e3884e
James Kim <yameskim@gmail.com>
2018-03-25 20:49:01 -0400

    Import skills images

--- galaxy-version/src/main.js
+++ galaxy-version/src/main.js
@@ -4,3 +4,29 @@
 // function draw() {
 //   background(255,255,255);
 // }
+window.onload = function() {
+  var forest = document.getElementById('forest');
+  var innerHeight = window.innerHeight;
+  console.log(innerHeight);
+
+  window.addEventListener('scroll', function(e) {
+    console.log(window.scrollY);
+    if (window.scrollY > innerHeight / 3) {
+      forest.style.display = 'inline-block';
+    } else {
+      forest.style.display = 'none';
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+}

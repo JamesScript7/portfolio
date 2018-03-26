628e8457061200bf85ac33a3b585ef0991f27f4c
James Kim <yameskim@gmail.com>
2018-03-26 07:17:55 -0400

    Fix js file

--- galaxy-version/src/main.js
+++ galaxy-version/src/main.js
@@ -1,21 +1,13 @@
-// function setup() {
-//   createCanvas(800,400);
-// }
-// function draw() {
-//   background(255,255,255);
-// }
 window.onload = function() {
   var forest = document.getElementById('forest');
   var wrapper = document.getElementById('wrapper');
-  var catPizza = document.getElementById('flying-cat-pizza');
   var innerHeight = window.innerHeight;
   // console.log(innerHeight);
-
   window.addEventListener('scroll', function(e) {
     // console.log(window.scrollY);
-    if (window.scrollY > (innerHeight / 6)) {
+    if (window.scrollY > (innerHeight / 9)) {
       forest.style.display = 'inline-block';
-      if (window.scrollY > (innerHeight / 4)) {
+      if (window.scrollY > (innerHeight / 8)) {
         wrapper.style.animation = 'fadeToBlack 1.5s forwards'
       } else {
         wrapper.style.animation = 'none';
@@ -24,18 +16,15 @@ window.onload = function() {
       wrapper.style.animation = 'none';
       forest.style.display = 'none';
     }
-
+    // var catPizza = document.getElementById('flying-cat-pizza');
     // if (catPizza.scrollIntoView()) {
     //   console.log('start the animation!')
     // }
   });
-
-
-
-
-
-
-
-
-
 }
+// function setup() {
+//   createCanvas(800,400);
+// }
+// function draw() {
+//   background(255,255,255);
+// }

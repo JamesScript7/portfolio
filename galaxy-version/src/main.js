8c88aa172ecf6466e588c8aaf5ba1f939f38c435
James Kim <yameskim@gmail.com>
2019-11-30 15:50:57 -0500

    cleanup

--- galaxy-version/src/main.js
+++ galaxy-version/src/main.js
@@ -17,26 +17,24 @@ window.addEventListener("DOMContentLoaded", function() {
     // Functions
     function isMobile() { return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Mobile|Silk|Opera Mini/i.test(navigator.userAgent)); }
 
-    // Handles impact page, arrow, forest, and cat animation set/reset.
+    // Handles impact page, forest, and cat animation set/reset.
     function animationCentral() {
         var scrollPosition = window.pageYOffset;
 
         // Evaluate when scrolled down 1/9 of window height
-        // else restore impact page to original, hide forest, and activate arrow jump animation.
+        // else restore impact page to original and hide forest
         if (scrollPosition > (innerHeight / 9)) {
             forest.style.display = 'inline-block';
-            // Fades out the impact page to black and removes arrow animation
+            // Fades out the impact page to black
             // when scrolled down 1/8 of the inner window height
             // else restore it back to original.
             if (scrollPosition > (innerHeight / 8)) {
                 wrapper.style.animation = 'fadeToBlack 1.5s forwards'
-                arrow.style.animation = 'none';
             } else {
                 wrapper.style.animation = 'none';
             }
         } else {
             wrapper.style.animation = 'none';
-            arrow.style.animation = 'jump 2.5s 1.5s ease-in-out';
             forest.style.display = 'none';
         }
 
